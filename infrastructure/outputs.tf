025e6df61e6ec0003edd5bb57815a6fb51c39b1e
Maev4l <maeval.nightingale@gmail.com>
2026-06-06 11:48:44 +0200

    chore: output new Lambda function ARNs

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -12,3 +12,13 @@ output "iam_role_arn" {
   description = "ARN of the Lambda IAM role"
   value       = module.renew_certificates.role_arn
 }
+
+output "lambda_dispatch_certificate_renewals_arn" {
+  description = "ARN of the dispatch-certificate-renewals Lambda function"
+  value       = module.dispatch_certificate_renewals.function_arn
+}
+
+output "lambda_handle_certificate_renewal_failure_arn" {
+  description = "ARN of the handle-certificate-renewal-failure Lambda function"
+  value       = module.handle_certificate_renewal_failure.function_arn
+}
